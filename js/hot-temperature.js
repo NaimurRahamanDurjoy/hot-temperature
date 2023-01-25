73fb2b35563bbb86007bfa8a35fcaca0ca026bed
NaimurRahamanDurjoy <dnaimurrahaman@gmail.com>
2023-01-25 20:23:45 +0600

    fetch weather api

--- js/hot-temperature.js
+++ js/hot-temperature.js
@@ -4,9 +4,16 @@ const apiKey = 'e3408a5b4f266319f2eb24a3f8163fcf';
 const weatherDataLoad = () => {
     // Location Search Field
     const searchField = document.getElementById('search-field');
-    const searchFieldValue = searchField.value;
+    const cityName = searchField.value;
     // clear search field
     searchField.value = '';
 
-    const url = ``;
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${apiKey}`;
+    fetch(url)
+    .then(res => res.json())
+    .then(data => displayCityWeather(data))
+}
+
+const displayCityWeather = city => {
+    console.log(city);
 }
