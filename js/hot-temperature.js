96f170fedb9e6ebf0f3281bd20fbaabe34f42781
NaimurRahamanDurjoy <dnaimurrahaman@gmail.com>
2023-01-25 17:37:27 +0600

    first commit

---
+++ js/hot-temperature.js
@@ -0,0 +1,12 @@
+// API_KEY
+const apiKey = 'e3408a5b4f266319f2eb24a3f8163fcf';
+
+const weatherDataLoad = () => {
+    // Location Search Field
+    const searchField = document.getElementById('search-field');
+    const searchFieldValue = searchField.value;
+    // clear search field
+    searchField.value = '';
+
+    const url = ``;
+}
