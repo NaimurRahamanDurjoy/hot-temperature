ee8ede8839c8ad41a9d1ff99e7becfb79dee184a
NaimurRahamanDurjoy <dnaimurrahaman@gmail.com>
2023-01-25 21:59:59 +0600

    add some comments with add async await

--- js/hot-temperature.js
+++ js/hot-temperature.js
@@ -1,7 +1,7 @@
 // API_KEY
 const apiKey = 'e3408a5b4f266319f2eb24a3f8163fcf';
-
-const weatherDataLoad = () => {
+// Load weather dada
+const weatherDataLoad = async () => {
     // Location Search Field
     const searchField = document.getElementById('search-field');
     const cityName = searchField.value;
@@ -9,13 +9,18 @@ const weatherDataLoad = () => {
     searchField.value = '';
 
     const url = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${apiKey}`;
-    fetch(url)
-    .then(res => res.json())
-    .then(data => displayCityWeather(data))
+    // fetch(url)
+    // .then(res => res.json())
+    // .then(data => displayCityWeather(data))
+    
+    // fetch by async await
+    const res = await fetch(url);
+    const data = await res.json();
+    displayCityWeather(data);
 }
-
+// Display weather details
 const displayCityWeather = city => {
-    console.log(city);
+    // get weather details div
     const cityWeatherDiv = document.getElementById('location-weather');
     cityWeatherDiv.innerHTML = `
         <img src="https://openweathermap.org/img/wn/${city.weather[0].icon}@2x.png" alt="">
